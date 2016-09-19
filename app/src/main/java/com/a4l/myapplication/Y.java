ab378595abbef7c941c010b30434ad2c85ed5bf5
ludagoga117 <luis.goyes@udea.edu.co>
2016-09-18 21:27:08 -0500

    Initial commit

---
+++ app/src/main/java/com/a4l/myapplication/Y.java
@@ -0,0 +1,38 @@
+package com.a4l.myapplication;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class Y extends Fragment {
+
+    Button cambiar;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_y, container, false);
+        // FIXME Se infla el fragmento en un objeto tipo view (rootView) luego saco las caracteristicas que necesito para finalmente retornarlo
+
+        cambiar = (Button) rootView.findViewById(R.id.button_fragY);
+
+        cambiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.contenedor_fragmentos, new X()).commit();
+            }
+        });
+
+        return rootView;
+    }
+
+}
